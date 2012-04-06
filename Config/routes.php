b97a8118998e1e6d35f77936160cd5d2bed15a9f
Brad Belyeu <bradleylamar@gmail.com>
2012-04-05 23:01:44 -0500

    Added some additional routes to make managing users easier

--- Config/routes.php
+++ Config/routes.php
@@ -1,25 +1,38 @@
 <?php
 
-    Router::connect('/login', array(
-        'plugin' => 'Authentication',
-        'controller' => 'Users',
-        'action' => 'login'
-    ));
+Router::connect('/login', array(
+    'plugin' => 'Authentication',
+    'controller' => 'Users',
+    'action' => 'login'
+));
 
-    Router::connect('/admin/login', array(
-        'plugin' => 'Authentication',
-        'controller' => 'Users',
-        'action' => 'login'
-    ));
+Router::connect('/admin/login', array(
+    'plugin' => 'Authentication',
+    'controller' => 'Users',
+    'action' => 'login'
+));
 
-    Router::connect('/logout', array(
-        'plugin' => 'Authentication',
-        'controller' => 'Users',
-        'action' => 'logout'
-    ));
+Router::connect('/logout', array(
+    'plugin' => 'Authentication',
+    'controller' => 'Users',
+    'action' => 'logout'
+));
 
-    Router::connect('/admin/logout', array(
-        'plugin' => 'Authentication',
-        'controller' => 'Users',
-        'action' => 'logout'
-    ));
+Router::connect('/admin/logout', array(
+    'plugin' => 'Authentication',
+    'controller' => 'Users',
+    'action' => 'logout'
+));
+
+Router::connect('/admin/users', array(
+    'plugin' => 'Authentication',
+    'controller' => 'Users',
+    'action' => 'index',
+    'prefix' => 'admin'
+));
+
+Router::connect('/admin/users/:action', array(
+    'plugin' => 'Authentication',
+    'controller' => 'Users',
+    'prefix' => 'admin'
+));
