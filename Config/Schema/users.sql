6e43521e38f53bb3dbc7c26b3bcb6ef6dcd8d8af
Brad Belyeu <bradleylamar@gmail.com>
2012-04-07 22:11:08 -0500

    Updated users.sql to add enum for role

--- Config/Schema/users.sql
+++ Config/Schema/users.sql
@@ -2,6 +2,7 @@ CREATE TABLE `users` (
     `id` int unsigned NOT NULL AUTO_INCREMENT,
     `username` varchar(50) NOT NULL COMMENT 'Users email address',
     `password` char(40) DEFAULT NULL COMMENT 'Sha1 hash of email',
+    `role` enum('Admin','Editor','Manager','User') default 'User',
     `created` datetime NOT NULL,
     `modified` datetime NOT NULL,
     PRIMARY KEY (`id`)
